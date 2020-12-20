97b6956cc525c1c62da9a1f56d121f0bdd8db968
Tiago Melo <tiagotele@gmail.com>
2020-12-19 22:24:20 -0300

    Adding failure cases to test examples. Set unittest to verbose mode. Using float numbers instead integers.

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -1,12 +1,14 @@
 from .shape import Shape
 
 class Rectangle(Shape):
-	width = 0
-	length = 0 
+	width = 0.0
+	length = 0.0 
 
 	def __init__(self, width, length):
 		self.width = width
 		self.length = length
 
 	def area(self):
+		if self.width <= 0.0 or self.length <= 0.0:
+			raise ValueError("That is not a positive number!")
 		return self.width * self.length
