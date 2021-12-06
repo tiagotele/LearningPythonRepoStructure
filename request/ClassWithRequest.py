c7b6eb43f9909483a6d52cf973376c40ee487a87
Tiago Melo <tiago.bmelo@stone.com.br>
2021-12-06 18:20:44 -0300

    Adding more unit tests for http requests

--- request/ClassWithRequest.py
+++ request/ClassWithRequest.py
@@ -3,6 +3,10 @@ import requests
 class ClassWithRequest():
 	
 	def get_from_url(self, id):
+		
+		if id is None:
+			return 'Bad Request'
+			
 		url=f'http://company.com/{id}'
 		x=requests.get(url)
 		if x.ok:
--- request/test_employee.py
+++ request/test_employee.py
@@ -4,15 +4,35 @@ from request.ClassWithRequest import ClassWithRequest
 
 class TestRequestExample(unittest.TestCase):
 	
-	def test_get_id(self):
+	def test_get_valid_id(self):
 		obj = ClassWithRequest()
 
 		with patch('request.ClassWithRequest.requests.get') as mocked_get:
 			mocked_get.return_value.ok = True
-			mocked_get.return_value.text='Success'
 
 			id=6
 			url=f'http://company.com/{id}'
 			result=obj.get_from_url(id)
 			mocked_get.assert_called_with(url)
 			self.assertEqual(result,'Success')
+	
+	def test_get_valid_id(self):
+		obj = ClassWithRequest()
+
+		with patch('request.ClassWithRequest.requests.get') as mocked_get:
+			mocked_get.return_value.ok = False
+
+			id=90
+			url=f'http://company.com/{id}'
+			result=obj.get_from_url(id)
+			mocked_get.assert_called_with(url)
+			self.assertEqual(result,'Bad Response')
+	
+	def test_get_none_id(self):
+		obj = ClassWithRequest()
+
+
+		id=None
+		url=f'http://company.com/{id}'
+		result=obj.get_from_url(id)
+		self.assertEqual(result,'Bad Request')
